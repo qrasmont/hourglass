d593f5f38721f33b35e166b8c2e2ffe57f2cd9d8
Quentin Rasmont <qrasmont@gmail.com>
2022-11-01 17:24:42 +0100

    timer: Add basic timer functionality

--- app/timer/timer.go
+++ app/timer/timer.go
@@ -1,6 +1,7 @@
 package timer
 
 import (
+	"fmt"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -12,17 +13,27 @@ func BackCmd() tea.Msg {
 	return BackMsg{}
 }
 
+type TickMsg struct{}
+
+func TickCmd() tea.Msg {
+	time.Sleep(1 * time.Second)
+	return TickMsg{}
+}
+
 type Model struct {
-	project     *tea.Model
-	span        time.Duration
-	description string
+	project      *tea.Model
+	current_span time.Duration
+	description  string
+	running      bool
+	last_time    time.Time
 }
 
 func New(project *tea.Model, description string) tea.Model {
 	m := Model{
-		project:     project,
-		span:        time.Duration(0),
-		description: description,
+		project:      project,
+		current_span: time.Duration(0),
+		description:  description,
+		running:      false,
 	}
 
 	return m
@@ -34,6 +45,7 @@ func (m Model) Init() tea.Cmd {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
 
@@ -42,12 +54,27 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		case "b", "esc":
 			return m, BackCmd
+		case "s":
+			m.running = !m.running
+
+			if m.running {
+				m.last_time = time.Now()
+				cmd = TickCmd
+			}
+		}
+
+	case TickMsg:
+		m.current_span += time.Now().Sub(m.last_time)
+		if m.running {
+			m.last_time = time.Now()
+			cmd = TickCmd
 		}
 	}
 
-	return m, cmd
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
 }
 
 func (m Model) View() string {
-	return "Timer view"
+	return "Project Name" + "\n" + "Spent: \n" + fmt.Sprintf("%s", m.current_span.Round(time.Second))
 }
