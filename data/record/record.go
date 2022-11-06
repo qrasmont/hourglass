62138b28fe0a86f6147c28f0dd9d2389be3da3ad
Quentin Rasmont <qrasmont@gmail.com>
2022-11-06 12:12:03 +0100

    record: Add multiple record query

--- data/record/record.go
+++ data/record/record.go
@@ -8,7 +8,10 @@ import (
 
 type Record struct {
 	gorm.Model
-	duration  time.Duration
+	Duration  time.Duration
+	Day       int
+	Month     time.Month
+	Year      int
 	ProjectID uint `gorm:"foreignKey:Project"`
 }
 
@@ -18,15 +21,32 @@ type GormRepository struct {
 
 type Repository interface {
 	CreateRecord(duration time.Duration, projectID uint) error
+	UpdateRecord(record Record) error
 	DeleteRecordForProject(projectID uint) error
 	DeleteRecordById(id uint) error
 	GetRecords(projectID uint) ([]Record, error)
+	GetRecordsForDay(date time.Time) (Record, error)
+	GetRecordsForMonth(date time.Time) ([]Record, error)
+	GetRecordsForYear(date time.Time) ([]Record, error)
+	GetRecordForProjectForDay(projectID uint, date time.Time) (Record, error)
+	GetRecordsForProjectForMonth(projectID uint, date time.Time) ([]Record, error)
+	GetRecordsForProjectForYear(projectID uint, date time.Time) ([]Record, error)
 }
 
-func (g *GormRepository) CreateRecord(duration time.Duration, projectID uint) error {
-	record := Record{duration: duration, ProjectID: projectID}
+func (g *GormRepository) CreateRecord(duration time.Duration, projectID uint, date time.Time) error {
+	record := Record{Duration: duration,
+		ProjectID: projectID,
+		Day:       date.Day(),
+		Month:     date.Month(),
+		Year:      date.Year(),
+	}
 
 	result := g.DB.Create(&record)
+	return result.Error
+}
+
+func (g *GormRepository) UpdateRecord(record Record) error {
+	result := g.DB.Save(&record)
 	return result.Error
 }
 
@@ -44,5 +64,41 @@ func (g *GormRepository) GetRecords(projectID uint) ([]Record, error) {
 	var records []Record
 
 	result := g.DB.Where("project_id = ?", projectID).Find(&records)
+	return records, result.Error
+}
+
+func (g *GormRepository) GetRecordsForDay(date time.Time) ([]Record, error) {
+	var records []Record
+	result := g.DB.Where(&Record{Day: date.Day(), Month: date.Month(), Year: date.Year()}).Find(&records)
+	return records, result.Error
+}
+
+func (g *GormRepository) GetRecordsForMonth(date time.Time) ([]Record, error) {
+	var records []Record
+	result := g.DB.Where(&Record{Month: date.Month(), Year: date.Year()}).Find(&records)
+	return records, result.Error
+}
+
+func (g *GormRepository) GetRecordsForYear(date time.Time) ([]Record, error) {
+	var records []Record
+	result := g.DB.Where(&Record{Year: date.Year()}).Find(&records)
+	return records, result.Error
+}
+
+func (g *GormRepository) GetRecordForProjectForDay(projectID uint, date time.Time) (Record, error) {
+	var record Record
+	result := g.DB.Where(&Record{ProjectID: projectID, Day: date.Day(), Month: date.Month(), Year: date.Year()}).Find(&record)
+	return record, result.Error
+}
+
+func (g *GormRepository) GetRecordsForProjectForMonth(projectID uint, date time.Time) ([]Record, error) {
+	var records []Record
+	result := g.DB.Where(&Record{ProjectID: projectID, Month: date.Month(), Year: date.Year()}).Find(&records)
+	return records, result.Error
+}
+
+func (g *GormRepository) GetRecordsForProjectForYear(projectID uint, date time.Time) ([]Record, error) {
+	var records []Record
+	result := g.DB.Where(&Record{ProjectID: projectID, Year: date.Year()}).Find(&records)
 	return records, result.Error
 }
